197f16531bc752b78de0db137b4de64fbc20da28
vitor56dantas <61641392+vitor56dantas@users.noreply.github.com>
2020-04-08 18:10:53 -0300

    Add files via upload

---
+++ ord/src/ord3/Ex5A.java
@@ -0,0 +1,28 @@
+package ord3;
+
+import java.util.Arrays;
+
+public class Ex5A {
+	public static void main(String[] args) {
+		int vet[] = { 4, 7, 1, 3, 2, 5};
+		System.out.println("Antes: " + Arrays.toString(vet));
+		insercao(vet);
+		System.out.println("Depois: " + Arrays.toString(vet));
+	}
+
+	public static void insercao(int vet[]) {
+		int p = 1;
+		for (int i = 1; i < vet.length; i++) {
+			int aux = vet[i];
+			int j = i - 1;
+			while (j >= 0 && vet[j] > aux) {
+				vet[j + 1] = vet[j];
+				System.out.println("Contagem: "+ p + Arrays.toString(vet));
+				j--;
+			}
+			p ++;
+			vet[j + 1] = aux;
+			System.out.println("Contagem: "+ p + Arrays.toString(vet));
+		}
+	}
+}
